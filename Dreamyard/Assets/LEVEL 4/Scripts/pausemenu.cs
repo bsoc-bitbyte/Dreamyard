0e0db4fbedbf8b936369a01a7000726970bdc848
Aadarsh_Nayak <162596882+Nayak578@users.noreply.github.com>
2024-07-18 23:31:22 +0530

    lv4

---
+++ Dreamyard/Assets/LEVEL 4/Scripts/pausemenu.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+                isPaused = !isPaused;
+            }
+
+            else
+            {
+                PauseGame();
+                isPaused = !isPaused;
+            }
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ResetLevel()
+    {
+        ResumeGame();
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        ResumeGame();
+    }
+
+    public void SettingsMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SettingsMenu");
+    }
+}
